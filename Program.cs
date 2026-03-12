ad744b52362cf6283fadd80a7807d3be4abc99af
Adonis <adrianspl@o2.pl>
2026-03-12 18:38:12 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace badanie_efektywnosc_algorytmow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
